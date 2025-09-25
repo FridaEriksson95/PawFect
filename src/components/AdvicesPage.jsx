04532287f7728cfedcbd048f0f572a2b46deea01
FridaEriksson95 <fridaeriiksson@gmail.com>
2025-09-25 09:24:50 +0200

    first try for screenorientation to fit all types no matter of size

---
+++ src/components/AdvicesPage.jsx
@@ -0,0 +1,21 @@
+import '../index.css'
+
+const AdvicesPage = () => {
+    return(
+        <> 
+        <h1 style={{ paddingTop:'40px'}}>🐾Goda Råd🐾</h1>
+        <p id='advice'>När din hund lämnas till oss ska den vara nykissad och redo för en härlig dag!</p>
+        <ul id='listServices'> 
+         <li><strong> Trött hund = lycklig hund </strong> – <br />en balanserad blandning av lek, promenader och vila gör underverk.</li><br />
+         <li><strong> Nosjobb är hjärnjympa</strong> – <br />låt hunden sniffa! Det tröttar ut mer än en lång jogg.</li><br />
+         <li><strong> Konsekvens är kärlek</strong> – <br />samma rutiner och regler ger trygghet.</li><br />
+         <li><strong> Socialisering är guld  </strong> – <br />hundar lär av varandra, precis som vi människor.</li><br />
+         <li><strong> Pälsvård är vardagslyx  </strong> – <br />en borstad päls är inte bara fin, den mår bättre också.</li><br />
+         <li><strong> Träning = bonding  </strong> – <br />små övningar stärker relationen mellan dig och din hund.</li><br />
+         <li><strong> Vila är heligt </strong> – <br />alla hundar, stora som små, behöver sina powernaps.</li><br />
+         <li><strong> Glädje smittar  </strong> – <br />ha kul tillsammans, hundar märker allt!</li>
+        </ul></>
+    )
+}
+
+export default AdvicesPage;
